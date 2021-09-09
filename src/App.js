1fb219b791667b99b8f34fdb29667efe13b2ae0e
unknown <jr.delacruz@media-meter.com>
2021-09-09 11:59:26 +0800

    my update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { IconButton, Input, InputAdornment, InputLabel } from '@material-ui/core';
+import React from 'react';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = React.useState({});
+
+  const getApi = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(function (response) {
+    // handle success
+    setUsers(response.data)
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center-form">
+     <div className="input-spacing"><TextField id="standard-basic" color="secondary" label="Email" /></div>
+     <div className="input-spacing"><TextField id="standard-basic" label="Username" /></div>
+     <div className="button-spacing"><Button variant="contained" onClick={getApi}>Default</Button></div>
+      {users.map((element, index) => (
+        <div className="userdata">
+         <h1>{element.name}</h1>
+         <div>{element.phone}</div>
+         </div>
+      ))}
+    </div>
     </div>
   );
 }
